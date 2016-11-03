59c39f527974477bd054d18e6e122d51a59312dd
apeckham <apeckham@gmail.com>
2016-11-03 13:18:25 -0700

    Include app.css

--- src/leinring_test/core.clj
+++ src/leinring_test/core.clj
@@ -1,11 +1,20 @@
 (ns leinring-test.core
   (:require [hiccup.core :refer :all]
-            [hiccup.page :refer :all])
+            [hiccup.page :refer :all]
+            [ring.middleware.resource :refer :all]
+            [ring.middleware.content-type :refer :all]
+            [ring.middleware.not-modified :refer :all])
   (:gen-class))
 
-(defn handler [request]
+(defn hello-world [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (html5
-            [:head]
-            [:h1 "Hello world" [:h2 "from leinring-test.core!"]])})
+          [:head (include-css "/app.css")]
+          [:h1 "Hello world" [:h2 "from leinring-test.core!"]])})
+
+(def handler
+  (-> hello-world
+      (wrap-resource "public")
+      (wrap-content-type)
+      (wrap-not-modified)))
